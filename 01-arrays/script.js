a4da077cfe1565c3ceb09645f81e0a2ff7a80932
its.john <giovannialesi87@icloud.com>
2025-02-22 16:08:48 +0100

    step da 6 a 9

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -16,37 +16,38 @@ teachers[4] = "Patrick"
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"];
 let lastTeachers = teachers.at(6)
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"];
-let teacher = teachers.at(1)
-console.log(teacher);
-
+const firstTeacher = [0];
+teachers.shift("nathan");
+console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push("Vanessa");
 console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-teachers.push("Sarah")
-teachers[0] = "Sarah"
+teachers.unshift("Sarah")
 console.log(teachers);
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"];
-let position = isFabioPresent.lastIndexOf("fabio") +1;
-// console.log(teachers);
+// const isFabioPresent = [];
+// let index = fruits.indexOf("Apple", -1);
+
 
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.includes("Lewis");
+console.log(5);
 
 // 9. Unisci tutti gli insegnanti nell'array teachers in una stringa
 // separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join();
+console.log(teachersString);
+
+
 
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
